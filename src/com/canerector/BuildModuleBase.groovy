f929f401b25dbb0ce7da9983fd97714ff8b8da72
James Fera <james.fera@gmail.com>
2016-12-28 14:18:55 -0500

    build modules

--- src/com/canerector/BuildModuleBase.groovy
+++ src/com/canerector/BuildModuleBase.groovy
@@ -46,12 +46,12 @@ abstract class BuildModuleBase{
 		pipeline.stage('Checkout') {
 			pipeline.checkout scm
 			
-			pipeline.bat 'git submodule update --init --recursive'
+			//pipeline.bat 'git submodule update --init --recursive'
             
-			//print 'Project Folder: ' + projectName							
+								
 			
-			pipeline.bat 'git checkout %BRANCH_NAME% && git pull'
-			pipeline.bat 'git remote remove origin1'  //this is for gitversion. it can't handle more than one remote
+			//pipeline.bat 'git checkout %BRANCH_NAME% && git pull'
+			//pipeline.bat 'git remote remove origin1'  //this is for gitversion. it can't handle more than one remote
 		}
 	}
 	

84669311433d7b309312a8b1699a1b34c1ff10c0
James Fera <james.fera@gmail.com>
2016-12-28 13:55:18 -0500

    build modules

--- src/com/canerector/DockerBuildModule.groovy
+++ src/com/canerector/DockerBuildModule.groovy
@@ -3,6 +3,7 @@ package com.canerector
 @groovy.transform.InheritConstructors
 class DockerBuildModule extends BuildModuleBase {
 		
+	@NonCPS
 	def performBuild(){
 		sendSlackMessage('building...')
 	}

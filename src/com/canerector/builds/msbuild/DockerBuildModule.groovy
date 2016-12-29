bfae1070fa265a7572289b612f84b8108ef918bd
James Fera <james.fera@gmail.com>
2016-12-29 13:17:15 -0500

    build modules

--- src/com/canerector/builds/msbuild/DockerBuildModule.groovy
+++ src/com/canerector/builds/msbuild/DockerBuildModule.groovy
@@ -2,10 +2,14 @@ package com.canerector.builds.msbuild
 
 def performBuild(buildContext){
 
+	echo 'Inside Module Project Name: ' + buildContext.projectName
+
 	def stage = new com.canerector.builds.msbuild.MsbuildStages(buildContext)	
 	def dockerStage = new com.canerector.builds.msbuild.DockerStages(buildContext)	
 
 	def cleanJobName = "${JOB_NAME}".replace('canerectors/', '')
+	
+	echo 'Inside Module Project Name1: ' + buildContext.projectName
 	
 	node{
 		ws ('c:\\' + cleanJobName){

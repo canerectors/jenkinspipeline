0911dba55d2a20252730fc9bb68871d618cd1119
James Fera <james.fera@gmail.com>
2017-02-24 14:37:54 -0500

    update nuget command

--- vars/nuget.groovy
+++ vars/nuget.groovy
@@ -5,7 +5,7 @@ def restore(){
 def publishPackage(packageName = '*.nupkg'){
 
 	withCredentials([string(credentialsId: 'nuget-api-key', variable: 'API_KEY')]) {
-        bat 'dotnet nuget push ' + packageName + ' --source https://www.myget.org/F/canerectors/api/v2/package --api-key %API_KEY% --symbols-source https://www.myget.org/F/canerectors/symbols/api/v2/package'                
+        bat 'dotnet nuget push ' + packageName + ' --source https://www.myget.org/F/canerectors/api/v2/package --api-key %API_KEY% --symbol-source https://www.myget.org/F/canerectors/symbols/api/v2/package --symbol-api-key %API_KEY%'
     }
 }
 

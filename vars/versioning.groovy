fd781d59e5c797a8cb3fd1804b636d61840892f8
James Fera <james.fera@gmail.com>
2017-02-24 12:16:03 -0500

    update version parsing

--- vars/versioning.groovy
+++ vars/versioning.groovy
@@ -1,6 +1,6 @@
 @NonCPS
 def getVersionFromBuildOutput() {
-    def matcher = manager.getLogMatcher("SemVer:(.*)\$") //manager.getLogMatcher(".*Setting version to: (.*)\$")
+    def matcher = manager.getLogMatcher(".*\"SemVer\":(.*)\$") //manager.getLogMatcher(".*Setting version to: (.*)\$")
 
 	if (matcher?.matches()) {
         def match = matcher.group(1).toString().replace('\"','')
